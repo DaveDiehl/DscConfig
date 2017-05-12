3b1711d9880c8928527d18c3c32b9a7746ba747f
Dave Diehl <dadiehl@gmail.com>
2017-05-12 10:43:33 -0400

    Initial commit

---
+++ TestConfig.ps1
@@ -0,0 +1,14 @@
+Configuration TeamCity
+{
+  param ($MachineName)
+
+  Node $MachineName
+  {
+       File CreateFile {
+           DestinationPath = "c:\Test.txt"
+           Ensure = "Present"
+           Contents = "Hello DSC from ARM"
+       }
+   }
+  }
+}
